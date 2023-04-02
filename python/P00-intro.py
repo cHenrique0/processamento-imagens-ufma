aeb4773e2690b1467560d8580893b91da85f1773
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-04-02 11:36:08 -0300

    introdução ao opencv

---
+++ python/P00-intro.py
@@ -0,0 +1,18 @@
+# Importando o OpenCV
+import cv2
+
+# Lendo a imagem
+img = cv2.imread("../img/Lenna.png")
+# [0] = largura, [1] = altura, [2] = canais
+largura, altura, canais = img.shape
+
+print(f"Largura: {largura} pixels")
+print(f"Altura: {altura} pixels")
+print(f"Canais: {canais}")
+
+# Mostrando a imagem
+cv2.imshow("Lenna", img)
+cv2.waitKey(0)  # Aguarda uma tecla ser pressionada para fechar a janela
+
+# Salvando a imagem
+# cv2.imwrite("../img/Lenna2.png", img)
