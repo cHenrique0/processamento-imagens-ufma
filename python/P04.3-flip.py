a27769f02c4466578ca638a6cb9e643f29732300
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-04-25 09:03:06 -0300

    espelhando imagens

---
+++ python/P04.3-flip.py
@@ -0,0 +1,21 @@
+import cv2
+
+image = cv2.imread("../img/input/Lenna.png")
+
+# Invertendo a imagem na horizontal
+flip_h = image[::-1, :]
+# flip_h = cv2.flip(image, 1) # Tem o mesmo efeito do slicing acima
+
+# Invertendo a imagem na vertical
+flip_v = image[:, ::-1]
+# flip_v = cv2.flip(image, 0)
+
+# Invertendo a imagem nos dois sentidos
+flip = image[::-1, ::-1]
+# flip = cv2.flip(image, -1)
+
+cv2.imshow("Original", image)
+cv2.imshow("Flip Horizontal", flip_h)
+cv2.imshow("Flip Vertical", flip_v)
+cv2.imshow("Flip", flip)
+cv2.waitKey(0)
