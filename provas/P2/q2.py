50e2e11589501163786364b478aa635266c6c795
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-06-19 21:54:57 -0300

    equalização de histograma v2

--- provas/P2/q2.py
+++ provas/P2/q2.py
@@ -1,41 +1,74 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-
-# Carregando a imagem usando o OpenCV
-img = cv2.imread('./images/ScenarioLowContrast.jpg', 0)
+from matplotlib import pyplot as plt
 
 
-# Equalização de histograma
-imgEq = cv2.equalizeHist(img)
+def calcHist(image):
+    # Criando o histograma com zeros
+    histogram = np.zeros(256, dtype=int)
 
-# Plotar histograma da imagem original
-originalHist = cv2.calcHist([img], [0], None, [256], [0, 256])
-plt.subplot(2, 2, 1)
-plt.bar(np.arange(256), originalHist.ravel(), color='b')
-plt.title('Histograma Original')
-plt.xlim([0, 256])
+    # Percorrendo os pixels da imagem e atualizando o histograma
+    for row in image:
+        for pixel in row:
+            histogram[pixel] += 1
 
-# Plotar imagem original
-plt.subplot(2, 2, 2)
-plt.imshow(img, cmap='gray')
-plt.title('Imagem Original')
-plt.axis('off')
+    return histogram
 
-# Plotar histograma da imagem equalizada
-eqHist = cv2.calcHist(
-    [imgEq], [0], None, [256], [0, 256])
-plt.subplot(2, 2, 3)
-plt.bar(np.arange(256), eqHist.ravel(), color='b')
-plt.title('Histograma Equalizado')
-plt.xlim([0, 256])
 
-# Plotar imagem equalizada
-plt.subplot(2, 2, 4)
-plt.imshow(imgEq, cmap='gray')
-plt.title('Imagem Equalizada')
-plt.axis('off')
+def calcCumulativeHist(histogram):
+    # Criando o histograma cumulativo com zeros
+    cumulativeHist = np.zeros(256, dtype=int)
 
-# Exibir o plot
-plt.tight_layout()
+    # Calculando o histograma cumulativo
+    cumulativeHist[0] = histogram[0]
+    for i in range(1, len(histogram)):
+        cumulativeHist[i] = cumulativeHist[i-1] + histogram[i]
+
+    return cumulativeHist
+
+
+def equalizeHist(image):
+    # Calculando o histograma da imagem
+    histogram = calcHist(image)
+
+    # Calculando o histograma cumulativo
+    cumulativeHist = calcCumulativeHist(histogram)
+
+    # Calculando o fator de normalização
+    numPixels = image.shape[0] * image.shape[1]
+    normalizationFactor = 255.0 / numPixels
+
+    # Equalizando a imagem
+    equalizedImage = np.zeros_like(image)
+    for i in range(len(cumulativeHist)):
+        equalizedValue = int(
+            round(cumulativeHist[i] * normalizationFactor))
+        equalizedImage[image == i] = equalizedValue
+
+    return equalizedImage
+
+
+# Carregando a imagem de entrada
+img = cv2.imread(
+    './images/ScenarioLowContrast.jpg')
+
+# Convertendo a imagem para escala de cinza
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Equalizando o histograma da imagem
+outputImg = equalizeHist(gray)
+
+# Exibindo a imagem original, a equalizada e seus respectivos histogramas
+plt.figure("Imagem original", figsize=(15, 10))
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.subplot(1, 2, 2)
+plt.hist(img.ravel(), 256, [0, 256], edgecolor="black")
+
+plt.figure("Imagem equalizada", figsize=(15, 10))
+plt.subplot(1, 2, 1)
+plt.imshow(outputImg, cmap="gray")
+plt.subplot(1, 2, 2)
+plt.hist(outputImg.ravel(), 256, [0, 256], edgecolor="black")
+
 plt.show()
